72692117270023e726127fb60c61ccd80cafe65b
jabrena <bren@juanantonio.info>
2020-04-18 20:32:18 +0200

    Travis process refactoring

---
+++ scripts/e2e_travis_tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Build docker
+docker build -t azure_function_image .
+docker run -p 80:80 --sysctl net.ipv6.conf.all.disable_ipv6=1 --name azure_function_image azure_function_image &
+sleep 40
+
+#e2e test
+assert "curl localhost:80/api/HttpTrigger-Java -d Azure" "Hello, Azure"
+
+#cleanup
+docker ps -a
+docker stop azure_function_image
+docker rm azure_function_image
