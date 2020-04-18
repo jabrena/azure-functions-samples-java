8abce116ee52836795e779144b4889b0a3107620
jabrena <bren@juanantonio.info>
2020-04-18 21:04:38 +0200

    Travis process refactoring

--- scripts/e2e_travis_tests.sh
+++ scripts/e2e_travis_tests.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+source ./scripts/assert.sh
+
 # Build docker
 docker build -t azure_function_image .
 docker run -p 80:80 --sysctl net.ipv6.conf.all.disable_ipv6=1 --name azure_function_image azure_function_image &
